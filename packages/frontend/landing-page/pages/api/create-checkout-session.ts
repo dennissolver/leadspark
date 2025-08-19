44167e74ae2078e424c5cfc2f7b7650d78ea9cf6
Dennis McMahon <mcmdennis@gmail.com>
2025-08-20 05:23:49 +1000

    Prod - Yhigh risk change made

--- packages/frontend/landing-page/pages/api/create-checkout-session.ts
+++ packages/frontend/landing-page/pages/api/create-checkout-session.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-06-20',
 });
 
 interface CreateCheckoutRequest {
